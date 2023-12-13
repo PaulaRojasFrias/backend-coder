9fbcb72e9267dde5aad8076f87beb73ea6ced9e6
Paula Rojas Frias <rojasfriaspaula@gmail.com>
2023-12-13 17:37:26 -0300

    desafio2 completado

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-class ProductManager {
-  constructor() {
-    this.products = [];
-  }
+const fs = require("fs").promises;
 
+class ProductManager {
   static ultimoId = 0;
 
-  addProduct(title, description, price, img, code, stock) {
+  constructor(path) {
+    this.products = [];
+    this.path = path;
+  }
+
+  async addProduct(nuevoObjeto) {
+    let { title, description, price, img, code, stock } = nuevoObjeto;
+
     if (!title || !description || !price || !img || !code || !stock) {
       console.log("Todos los campos son obligatorios");
       return;
@@ -27,68 +32,124 @@ class ProductManager {
     };
 
     this.products.push(newProduct);
+
+    await this.guardarArchivo(this.products);
   }
 
   getProducts() {
     console.log(this.products);
   }
 
-  getProductsById(id) {
-    const product = this.products.find((item) => item.id === id);
+  async getProductsById(id) {
+    try {
+      const arrayProductos = await this.leerArchivo();
+      const buscado = arrayProductos.find((item) => item.id === id);
 
-    if (!product) {
-      console.log("No se ha encontrado el producto");
-    } else {
-      console.log("Se ha encontrado el producto: ", product);
+      if (!buscado) {
+        console.log("No se ha encontrado el producto");
+      } else {
+        console.log("Se ha encontrado el producto: ");
+        return buscado;
+      }
+    } catch (error) {
+      console.log("Se produjo un error al leer el archivo ", error);
     }
-    return product;
+  }
+
+  async leerArchivo() {
+    try {
+      const respuesta = await fs.readFile(this.path, "utf-8");
+      const arrayProductos = JSON.parse(respuesta);
+      return arrayProductos;
+    } catch (error) {
+      console.log("Se produjo un error al leer el archivo", error);
+    }
+  }
+
+  async guardarArchivo(arrayProductos) {
+    try {
+      await fs.writeFile(this.path, JSON.stringify(arrayProductos, null, 2));
+    } catch (error) {
+      console.log("Error al guardar el archivo", error);
+    }
+  }
+
+  async updateProduct(id, productoActualizado) {
+    try {
+      const arrayProductos = await this.leerArchivo();
+      const index = arrayProductos.findIndex((item) => item.id === id);
+
+      if (index !== -1) {
+        arrayProductos.splice(index, 1, productoActualizado);
+        await this.guardarArchivo(arrayProductos);
+      } else {
+        console.log("No se encontro el producto");
+      }
+    } catch (error) {
+      console.log("Error al actualizar el producto", error);
+    }
   }
 }
 
-const manager = new ProductManager();
+//Testing
+
+const manager = new ProductManager("./productos.json");
 
 manager.getProducts();
 
-manager.addProduct(
-  "producto prueba",
-  "Este es un producto prueba",
-  200,
-  "Sin imagen",
-  "abc123",
-  25
-);
+const productoPrueba = {
+  title: "producto prueba",
+  description: "Este es un producto prueba",
+  price: 200,
+  img: "Sin imagen",
+  code: "abc123",
+  stock: 25,
+};
+
+manager.addProduct(productoPrueba);
+
+const zapatillasConverse = {
+  title: "zapatillas converse",
+  description: "Color rojo",
+  price: 5000,
+  img: "Sin imagen",
+  code: "fff333",
+  stock: 3,
+};
+
+manager.addProduct(zapatillasConverse);
+
+const zapatillasNike = {
+  title: "zapatillas nike",
+  description: "Color negro",
+  price: 8000,
+  img: "Sin imagen",
+  code: "eee222",
+  stock: 5,
+};
+manager.addProduct(zapatillasNike);
 
 manager.getProducts();
 
-//comprobando validaciones
-manager.addProduct(
-  "producto prueba",
-  "Este es un producto prueba",
-  200,
-  "Sin imagen",
-  "abc123",
-  25
-);
+async function testeamosBusquedaPorId() {
+  const buscado = await manager.getProductsById(2);
+  console.log(buscado);
+}
 
-manager.addProduct(
-  "producto prueba",
-  "Este es un producto prueba",
-  200,
-  "Sin imagen",
-  "abc321"
-);
+testeamosBusquedaPorId();
 
-manager.addProduct(
-  "zapatillas converse",
-  "Color rojo",
-  5000,
-  "Sin imagen",
-  "fff333",
-  3
-);
+const zapatillasFila = {
+  id: 1,
+  title: "zapatillas fila",
+  description: "color blanco",
+  price: 10000,
+  img: "Sin imagen",
+  code: "iii999",
+  stock: 10,
+};
 
-manager.getProducts();
+async function testeamosActualizar() {
+  await manager.updateProduct(1, zapatillasFila);
+}
 
-manager.getProductsById(2);
-
-manager.getProductsById(5);
+testeamosActualizar();
