64d29c8f94b09c1b904923d13e3f8f21b2f2434c
Paula Rojas Frias <rojasfriaspaula@gmail.com>
2023-12-24 13:22:31 -0300

    ultimos cambios

--- src/app.js
+++ src/app.js
@@ -4,18 +4,12 @@ const express = require("express");
 
 const app = express();
 
-//productos
+//Productos
 const ProductManager = require("./productManager");
 
-const manager = new ProductManager("./productos.json");
+const manager = new ProductManager("./src/productos.json");
 
-const misProductos = manager.getProducts();
-
-//creando ruta
-
-app.get("/", (req, res) => {
-  res.send("Bienvenido a la tienda");
-});
+//Puerto
 
 const PUERTO = 8080;
 
@@ -23,29 +17,43 @@ app.listen(PUERTO, () => {
   console.log(`Escuchando con express en http://localhost:${PUERTO}`);
 });
 
-//rutas
+//Rutas
 
-//limite
-app.get("/products", (req, res) => {
-  let limit = parseInt(req.query.limit);
+app.get("/", (req, res) => {
+  res.send("Bienvenido a la tienda");
+});
 
-  if (limit) {
-    const products = misProductos.slice(0, limit);
-    res.send(products);
-  } else {
-    res.send(misProductos);
+//Limite
+app.get("/products", async (req, res) => {
+  try {
+    const misProductos = await manager.leerArchivo();
+    let limit = parseInt(req.query.limit);
+    if (limit) {
+      const products = misProductos.slice(0, limit);
+      return res.send(products);
+    } else {
+      return res.send(misProductos);
+    }
+  } catch (error) {
+    console.log(error);
+    throw new Error("Se produjo un error al procesar la solicitud.");
   }
 });
 
-//con id
+//Con id
 
-app.get("/products/:id", (req, res) => {
-  let id = req.params.id;
-  const product = misProductos.find((item) => item.id == id);
+app.get("/products/:id", async (req, res) => {
+  try {
+    let id = parseInt(req.params.id);
+    const product = await manager.getProductsById(id);
 
-  if (product) {
-    res.send(product);
-  } else {
-    res.send("Producto no encontrado");
+    if (product) {
+      return res.send(product);
+    } else {
+      return res.send("Producto no encontrado");
+    }
+  } catch (error) {
+    console.log(error);
+    throw new Error("Se produjo un error al procesar la solicitud.");
   }
 });
