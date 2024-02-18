2321b650ae22834a593aa892c499ebf34473d121
Paula Rojas Frias <rojasfriaspaula@gmail.com>
2024-02-18 19:44:31 -0300

    views router

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,8 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const CartManager = require("../dao/db/cart-manager-db.js");
+const cartManager = new CartManager();
 
 router.get("/", async (req, res) => {
   res.send("Bienvendido a la tienda!");
+});
+
+router.get("/carts/:cid", async (req, res) => {
+  const cartId = req.params.cid;
+
+  try {
+    const cart = await cartManager.getCartByid(cartId);
+
+    if (!cart) {
+      console.log("No existe carrito con ese id");
+      return res.status(404).json({ error: "Carrito no encontrado" });
+    }
+
+    const cartProducts = cart.products.map((item) => ({
+      product: item.product.toObject(),
+      quantity: item.quantity,
+    }));
+
+    res.render("carts", { products: cartProducts });
+  } catch (error) {
+    console.error("Error al obtener el carrito", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
 });
 
 module.exports = router;
