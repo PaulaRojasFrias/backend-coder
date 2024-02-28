5fd52bf5e614ca9f5b5679bd62e0aac65c453aac
Paula Rojas Frias <rojasfriaspaula@gmail.com>
2024-02-28 17:30:49 -0300

    users

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ const PUERTO = 8080;
 const socket = require("socket.io");
 const exphbs = require("express-handlebars"); //Handlebars
 require("./database.js"); //mongoose
+//session
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
@@ -12,15 +15,29 @@ app.set("views", "./src/views");
 //Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(
+  session({
+    secret: "secretCoder",
+    resave: true,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl:
+        "mongodb+srv://paularojasfrias5:JDDAXiu1gqZGhQEE@cluster0.rc2dfjg.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0",
+      ttl: 90,
+    }),
+  })
+);
 
 //Rutas
 const productsRouter = require("./routes/products.router.js");
 const cartsRouter = require("./routes/carts.router.js");
 const viewsRouter = require("./routes/views.router.js");
+const userRouter = require("./routes/user.router.js");
 
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
 app.use("/", viewsRouter);
+app.use("/api/users", userRouter);
 
 app.use(express.static("./src/public"));
 
